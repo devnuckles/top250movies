1705d68883571549a525ee6b9a1553e5f6e8039c
devnuckles <mohammadroman925@gmail.com>
2024-06-22 12:36:59 +0600

    Added Student Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Movies from "./platform/movies/movies";
+import StudentList from "./platform/students/studentList";
 
 const App = () => {
     return (
         <div className="container">
+            <StudentList />
             <Movies />
         </div>
     );
